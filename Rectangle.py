160c72f93bd5f92e621c9bfb17f073f4e323dd47
speedyromans <rouquier.pascal@wanadoo.fr>
2021-07-06 10:38:42 +0200

    Ajout appel de la classe

--- Rectangle.py
+++ Rectangle.py
@@ -1,13 +1,21 @@
-'Programation d une classe'
 
+#  appel des Bibliotheques utile à la classe
+
+from numpy import sqrt as racine
+
+#'Programation de la classe'
 class rectangle:
+    # Affectation des variables d'entrée 
     longueur = 3
     largeur =4
 
     def calculate_area(self):
         return self.largeur * self.longueur
     def calculate_diagonale(self):
-        return self.largeur * self.longueur
+        diagonale= racine(self.largeur^2 +self.longueur^2)
+        return diagonale
+    def calculate_perimeter(self):
+        return 2*(self.largeur + self.longueur)
 
     
 
---
+++ calcul.py
@@ -0,0 +1,9 @@
+#Programme pour utiliser la classe Rectangle
+from pythonObject.Rectangle import rectangle
+
+
+long=rectangle.longueur
+print(long)
+
+
+
