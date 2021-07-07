f5f857b044c92f5b3dfd71dd90190d14c75123e6
speedyromans <rouquier.pascal@wanadoo.fr>
2021-07-07 14:37:47 +0200

    Avec Constructeur et instance d'objet

--- Rectangle.py
+++ Rectangle.py
@@ -1,21 +1,18 @@
 
 #  appel des Bibliotheques utile à la classe
-from numpy import sqrt as racine
+from numpy import sqrt
 
 #'Programation de la classe'
 class Rectangle:
     # Affectation des variables d'entrée avec la methode magique
-    def __init__(self,longueur, largeur, surface, diagonale, perimetre):
+    def __init__(self,longueur, largeur):
         self.longueur = longueur
         self.largeur = largeur
-        self.surface = surface
-        self.diagonale = diagonale
-        self.perimetre = perimetre
-
+     
     def calculate_area(self):
         return self.largeur * self.longueur
     def calculate_diagonale(self):
-        diagonale= racine(self.largeur^2 +self.longueur^2)
+        diagonale= sqrt(self.largeur**2 +self.longueur**2)
         return diagonale
     def calculate_perimeter(self):
         return 2*(self.largeur + self.longueur)
--- calcul.py
+++ calcul.py
@@ -1,12 +1,13 @@
 #Programme pour utiliser la classe Rectangle
-from Rectangle import Rectangle
 
-r = Rectangle()
-long=r.longueur
-print(long)
+import Rectangle
+#L'instance du rectangle de cote 4 et 3 s'appelle r
+r = Rectangle.Rectangle(4,3)
+
 area=r.calculate_area()
 print("La surface est de ",area)
-
+print ("La diagonale mesure ",r.calculate_diagonale())
+print("Le perimetre mesure ", r.calculate_perimeter())
 
 
 
